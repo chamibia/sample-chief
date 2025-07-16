4f756ad4395314c76522155788ce54e50af320cb
Bianca Chami <b.chami39@gmail.com>
2025-07-16 16:19:55 -0400

    updated variables

--- samp-chief/src/lib/shopify.ts
+++ samp-chief/src/lib/shopify.ts
@@ -1,9 +1,9 @@
 import { createStorefrontApiClient } from '@shopify/storefront-api-client';
 
 const client = createStorefrontApiClient({
-  storeDomain: process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN!,
+  storeDomain: process.env.SHOPIFY_STORE_DOMAIN!,
   apiVersion: '2024-01',
-  publicAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN!,
+  publicAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN!,
 });
 
 export { client };
