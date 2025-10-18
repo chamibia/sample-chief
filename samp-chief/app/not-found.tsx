b7e1e86c817635716ab312bdc95a8aba9c429120
Tamlyn <tamlyn.lou@gmail.com>
2025-10-18 17:10:38 -0400

    updates

---
+++ samp-chief/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: 48, textAlign: 'center' }}>
+      <h1 style={{ fontSize: 28, marginBottom: 12 }}>Page not found</h1>
+      <p>The page you requested could not be found.</p>
+    </div>
+  );
+}
